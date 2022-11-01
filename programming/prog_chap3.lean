7c64e6f05454463f32c17e080c29dca04a935684
David Vodička <dave@vodickovi.cz>
2022-11-01 21:15:14 +0100

    Proof of small index eixstence from h1

--- programming/prog_chap3.lean
+++ programming/prog_chap3.lean
@@ -7,3 +7,9 @@ example : "Hello".append " lean" = "Hello lean" := rfl
 example : 5 < 8 := by simp
 
 def getFiftElem (x : List α) (h : x.length > 5) := x[5]'h
+
+def getSixthElem (x : List α) (h1 : x.length > 10) := x[6]'h2
+  where h2 : x.length > 6 := by
+have h : 6 < 10 := by simp
+apply Nat.lt_trans h
+exact h1
